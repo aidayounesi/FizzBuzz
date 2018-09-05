ed52bcb5449e8ab80884c4dafee412651b3621a5
Aida Younesi <Aida.Younesi@softwire.com>
2018-09-05 17:40:33 +0100

    code refactoring with help of Mike

--- C#/ConsoleApp/ConsoleApp/Program.cs
+++ C#/ConsoleApp/ConsoleApp/Program.cs
@@ -10,56 +10,53 @@ namespace ConsoleApp
         static void Main(string[] args)
         {
             Console.WriteLine("Please enter maximum number for extended FizzBuzz game: ");
-            var maxNum = Console.Read();
+            Int32.TryParse(Console.ReadLine(), out var maxNum);
 
             var fizzBuzzer = new FizzBuzzGame(1, maxNum);
 
-
-            foreach (var value in fizzBuzzer)
+            foreach (var value in fizzBuzzer.Play())
             {
                 Console.WriteLine(value);
             }
         }
     }
 
-    class FizzBuzzGame : IEnumerable<string>
+    class FizzBuzzGame
     {
-        private int _minNum = 1;
-        private int _maxNum = 100;
-
-        private List<string> StringResults = new List<string>();
-
-        public FizzBuzzGame() {}
-
-        public FizzBuzzGame(int minNum, int maxNum)
+        private int _minNum;
+        private int _maxNum;
+        
+        public FizzBuzzGame(int minNum = 1, int maxNum = 100)
         {
             _minNum = minNum;
             _maxNum = maxNum;
-
-            Play();
         }
 
-        static readonly Dictionary<int, KeyValuePair<Func<List<string>, string, List<string>>, string>> Rules =
-            new Dictionary<int, KeyValuePair<Func<List<string>, string, List<string>>, string>>()
+        static readonly Dictionary<int, Action<List<string>>> Rules = new Dictionary<int, Action<List<string>>>()
             {
-                {3, new KeyValuePair<Func<List<string>, string, List<string>>, string>(AppendLast, "Fizz")},
-                {5, new KeyValuePair<Func<List<string>, string, List<string>>, string>(AppendLast, "Buzz")},
-                {7, new KeyValuePair<Func<List<string>, string, List<string>>, string>(AppendLast, "Bang")},
-                {11, new KeyValuePair<Func<List<string>, string, List<string>>, string>(ExclusiveCase, "Bong")},
-                {13, new KeyValuePair<Func<List<string>, string, List<string>>, string>(AppendBeforeB, "Fezz")},
-                {17, new KeyValuePair<Func<List<string>, string, List<string>>, string>(Reverse, "N/A")}
+                {3,  CreateUpdateFunc(AppendLast, "Fizz")},
+                {5,  CreateUpdateFunc(AppendLast, "Buzz")},
+                {7,  CreateUpdateFunc(AppendLast, "Bang")},
+                {11, CreateUpdateFunc(ExclusiveCase, "Bong")},
+                {13, CreateUpdateFunc(AppendBeforeB, "Fezz")},
+                {17, CreateUpdateFunc(Reverse, null)}
             };
-        
-        private static List<string> AppendLast(List<string> generatedStringsList, string keyword)
+
+        private static Action<List<string>> CreateUpdateFunc(Action<List<string>, string> updateBehaviour, string keyword)
+        {
+            return (existingStrings) => updateBehaviour(existingStrings, keyword);
+        }
+
+        private static void AppendLast(List<string> generatedStringsList, string keyword)
         {
             generatedStringsList.Add(keyword);
-            return generatedStringsList;
         }
 
 
-        private static List<string> ExclusiveCase(List<string> generatedStringsList, string keyword)
+        private static void ExclusiveCase(List<string> generatedStringsList, string keyword)
         {
-            return new List<string> {keyword};
+            generatedStringsList.Clear();
+            generatedStringsList.Add(keyword);
         }
 
         private static int FindIndexCharBeginInStrArray(List<string> givenList, char givenChar)
@@ -67,56 +64,48 @@ namespace ConsoleApp
             return givenList.FindIndex(stringToCheck => stringToCheck[0] == givenChar);
         }
 
-        private static List<string> AppendBeforeB(List<string> generatedStringsList, string keyword)
+        private static void AppendBeforeB(List<string> generatedStringsList, string keyword)
         {
             int indexB = FindIndexCharBeginInStrArray(generatedStringsList, 'B');
             if (indexB >= 0)
             {
                 generatedStringsList.Insert(indexB, keyword);
-                return generatedStringsList;
             }
-
-            return AppendLast(generatedStringsList, keyword);
+            else
+            {
+                AppendLast(generatedStringsList, keyword);
+            }
         }
 
-        private static List<string> Reverse(List<string> generatedStringsList, string keyword)
+        private static void Reverse(List<string> generatedStringsList, string keyword)
         {
             generatedStringsList.Reverse();
-            return generatedStringsList;
         }
 
-        public void Play()
+        public IEnumerable<string> Play()
         {
             for (int i = _minNum; i < _maxNum; i++)
             {
-                List<string> strList = new List<string>();
+                List<string> returnStrings = new List<string>();
                 // for each number applies all the suitable rules
-                foreach (KeyValuePair<int, KeyValuePair<Func<List<string>, string, List<string>>, string>> rule in Rules)
+                foreach (var rule in Rules)
                 {
+                    var divisor = rule.Key;
+                    var stringsUpdateFunc = rule.Value;
                     // if this number is dividable by the key, so the rule should be applied
-                    if (i % rule.Key == 0)
+                    if (i % divisor == 0)
                     {
-                        strList = rule.Value.Key(strList, rule.Value.Value);
+                        stringsUpdateFunc(returnStrings);
                     }
                 }
 
                 // if no rules has been applied, the number itself should be printed
-                if (strList.Count == 0)
+                if (!returnStrings.Any())
                 {
-                    strList.Add(i.ToString());
+                    returnStrings.Add(i.ToString());
                 }
-               StringResults.Add(String.Join("", strList));
+                yield return String.Join("", returnStrings);
             }
-        }
-
-        public IEnumerator<string> GetEnumerator()
-        {
-            return StringResults.GetEnumerator();
-        }
-
-        IEnumerator IEnumerable.GetEnumerator()
-        {
-            return GetEnumerator();
         }
     }
 }
